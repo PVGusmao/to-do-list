c3cea6ec2f89c33af87962571bd1cc290b089f06
Paulo Gusmao <paulo.gusmao@grupoaguia.corp>
2022-11-17 14:24:28 -0300

    algumas rotas implementadas

--- src/index.js
+++ src/index.js
@@ -1,33 +1,78 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if(!user) return response.status(400).json({ error: "Customer not Found!" });
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const newUser = {
+    id: uuidv4(),
+    name, 
+    username, 
+    todos: []
+  }
+
+  users.push(newUser);
+
+  return response.status(201).json(newUser);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  return response.status(200).json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { user } = request;
+
+  const task = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date(),
+  }
+
+  user.todos.push(task);
+
+  return response.status(201).json(task);
+
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { id } = request.params;
+
+  const { user } = request;
+
+  const updateTask = user.todos.find((task) => task.id === id);
+
+  updateTask.deadline = deadline;
+  updateTask.title = title;
+
+  return response.status(200).send('Atualizado com sucesso.')
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
